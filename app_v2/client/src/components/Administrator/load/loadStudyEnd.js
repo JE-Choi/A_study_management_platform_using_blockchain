b8ae5a21e1e64415adec5150282fe95dd8e83b85
최지은 <lielieing@naver.com>
2019-11-30 05:01:34 +0900

    유지보수

--- app_v2/client/src/components/Administrator/load/loadStudyEnd.js
+++ app_v2/client/src/components/Administrator/load/loadStudyEnd.js
@@ -54,8 +54,6 @@ const loadStudyEnd = {
                         let transactions_web3_date = InitContract.web3.utils.hexToUtf8(transactions[3]);
                         let transactions_web3_time = InitContract.web3.utils.hexToUtf8(transactions.time);
                         
-                        console.log(transactions.idx_hash);
-    
                         DBControl_txn.callSelectTxnInfo(transactions.idx_hash.substr(2)).then((res)=>{
                             if(res.data.length > 0){
                                 let txn_hash = '0x'+res.data[0].txn_hash;
@@ -75,7 +73,7 @@ const loadStudyEnd = {
                                         } 
                                     }
                                 } else {
-                                    let date = new Date(transactions_web3_date+" 00:00:00");
+                                    let date = new Date(transactions_web3_date + ' ' + transactions_web3_time);
                                     transactions_list_sub.push(date, 'StudyEnd',transactions_web3_date, txn_hash, transactions_web3_personId,transactions_web3_personName, transactions_web3_coin, _study_id);
                                     transactions_list.push(transactions_list_sub);
                                     
